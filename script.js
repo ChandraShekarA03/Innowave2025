b3a4df8a86685dc74b50091ed73bf119b3da3d6f
NightFuryyyyy <thejus.thomas@bcah.christuniversity.in>
2025-04-22 14:44:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-window.addEventListener("scroll", () => {
-    const heroLogo = document.getElementById("hero-logo");
-    const target = document.getElementById("logo-target");
+// window.addEventListener("scroll", () => {
+//     const heroLogo = document.getElementById("hero-logo");
+//     const target = document.getElementById("logo-target");
   
-    const scrollThreshold = 100;
+//     const scrollThreshold = 100;
   
-    if (window.scrollY > scrollThreshold) {
-      const targetRect = target.getBoundingClientRect();
-      const x = targetRect.left;
-      const y = targetRect.top;
+//     if (window.scrollY > scrollThreshold) {
+//       const targetRect = target.getBoundingClientRect();
+//       const x = targetRect.left;
+//       const y = targetRect.top;
   
-      heroLogo.style.transform = `translate(${x}px, ${y}px) scale(0.4)`;
-    } else {
-      heroLogo.style.transform = `translate(50vw, 50vh) scale(1)`;
-    }
-  });
+//       heroLogo.style.transform = `translate(${x}px, ${y}px) scale(0.4)`;
+//     } else {
+//       heroLogo.style.transform = `translate(50vw, 50vh) scale(1)`;
+//     }
+//   });
   
